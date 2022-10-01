11cc25e709b7dd0fe78a2060564a670ba3d7e9de
Nishant Kumar <nishant_kumar@epam.com>
2022-10-01 22:51:11 +0530

    gmail raw client

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"context"
+	"expentracker/gateway"
+	"log"
+
+	"google.golang.org/api/gmail/v1"
+)
+
 func main() {
+	ctx := context.Background()
+	gmailService := getGmailService(ctx)
+	log.Println("gmail serivce = ", gmailService)
+
 	//hello
+}
+
+func getGmailService(ctx context.Context) *gmail.Service {
+	gatewayClient := gateway.NewGateway(ctx)
+	gmailService, err := gatewayClient.GmailClient()
+	if err != nil {
+		log.Println("Error = ", err.Error())
+	}
+	return gmailService
 }
